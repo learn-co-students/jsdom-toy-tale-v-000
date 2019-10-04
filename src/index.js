5624c68a65ed89f9121db69ae6556b78c2561029
github username <github email address>
2019-10-04 17:32:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,78 +1,131 @@
-const addBtn = document.querySelector('#new-toy-btn')
+const addNewToyButton = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const addToyForm = document.querySelector('.add-toy-form')
 let addToy = false
 
-// YOUR CODE HERE
-
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-  } else {
-    toyForm.style.display = 'none'
-  }
-})
 
 
-const getAllToys = () => {
-	return fetch('http://localhost:3000/toys')
-		.then(response => response.json())
-		.then(json => console.log(json))
-}
-
-
- const postNewToy = (data) => {
+const addNewToy = (toy) => { //toy = event.target
+    debugger
     const url = "http://localhost:3001/toys"
-    let configObject = {
+    let configObjectPost = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
         },
+
         body: JSON.stringify({
-            'name': toyData.name.value,
-            'image': toyData.image.value,
+            'name': toy.name.value,
+            'image': toy.image.value,
             'likes': 0
         })
     }
-	fetch(url, configObject )
+
+	fetch(url, configObjectPost )
 		.then(response => response.json())
-		.then(data => createToyCard(data))
+		.then(data => {
+			createToyCard(data)
+		})
 		.catch(error => console.log(error.message))
 }
 
+addNewToyButton.addEventListener('click', (event) => {
+  // hide & seek with the form
+  addToy = !addToy
+  if (addToy) {
+    toyForm.style.display = 'block';
+    //do something to capture the data and go to POST/
+    addToyForm.addEventListener('submit', e => {
+    	e.preventDefault()
+	    addNewToy(event.target)
+		})
+  	} else {
+    toyForm.style.display = 'none'
+  }
+})
+
+//Fetch and display the toys
+const getAllToys = () => {
+	 fetch('http://localhost:3000/toys')
+		.then(response => response.json())
+	
+		.then(json => listAllToys(json))
+		// .then(json => console.log(json))
+}
+
+//<div class='card'>
+// <h2> `${toy.name}` </h2>
+//<img src=`${data.image}` class="toy-avatar"></img>
+//<p> `${toy.likes} likes` </p>
+//<button class="like-btn">Like`</button>
+//</div
+
+const listAllToys = (toys) => {
+	toys.forEach(toy => createToyCard(toy))
+}
+
 const createToyCard = (toy) => {
+	//add for each toy in toy? 
+	let div = document.createElement('div')
+	div.classname = "card"
+
 	let h2 = document.createElement('h2')
-	h2.innerText = toy.name
+	h2.innerText = `${toy.name}`
 
 	let img = document.createElement('img')
-	img.src = data.image
-	img.classname = "toy-avatar"
+	img.src = toy.image
+	img.setAttribute('class', "toy-avatar")
 	
 	let p = document.createElement('p')
-	p.likes = ''
+	p.setAttribute('class', "toy-likes")
+	p.innerHTML = `${toy.likes} likes`
 
 	let button = document.createElement('button')
-	button.classname = 'like-btn'
-	btn.innerText = "like"
-	btn.addEventListener('click', (event) => {
-		console.log("not sure what to do")
-		likes(event)
+	button.setAttribute('class', 'like-button')
+	button.setAttribute('id', toy.id)
+	button.innerText = 'Like'
+	button.addEventListener('click', (event) => {
+		updateLikes(event)
 	})
-//.setAttribute(attrib, name) diff between this and simple dot notation and = 
+
+	div.append(h2, img, p, button)
+	toyCollection = document.querySelector('#toy-collection')
+	toyCollection.appendChild(div)
+	
+}
+
+window.addEventListener('DOMContentLoaded', getAllToys)
+
+
+ 
+
+
+const updateLikes = (event) => {
+	const url = `http://localhost:3000/toys/${event.target.id}`
+	const configObjectPatch = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            'likes': '' //figure out how to capture current likes 
+        })
+    }
+
+	fetch(url, configObjectPatch)
+		.then(response => response.json())
+		.then(data => howManyLikes(data))
 
 }
 
-const addToDom = (data) => {
-	const div = document.createElement('div')
-	div.classname = "card"
-	const putToysHere = document.querySelector("#toy-collection")
-	putToysHere.append.div
 
+const howManyLikes = (data) => {
+	console.log(data.likes) 
 }
 
 
-	//make a div
-	//give it a classname attribute card 
-	//add to toy-collection div 
+
+
+	
