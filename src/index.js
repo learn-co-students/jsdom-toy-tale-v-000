55de99dd849f4d88e688fd6fd03db95873eda68f
AlwinaO <aoyewole2586@gmail.com>
2019-10-20 22:23:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetchToys();
+})
+
+
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
@@ -16,3 +21,39 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+const fetchToys = () => {
+  const toyUrl = "http://localhost:3000/toys";
+  fetch(toyUrl)
+    .then((response) => response.json())
+    .then((json) => renderToys(json))
+}
+
+const renderToys = (json) => {
+  const div = document.getElementById('toy-collection');
+  Object.keys(json).forEach((toy) => {
+    console.log(toy); //logs 0 in console or if toy["name"] or toy.name, it's undefined
+    const toyDivCard = document.createElement('div');
+    toy = toyDivCard.classList.add('toy-card');
+
+    const h2ToyName = document.createElement('h2');
+    h2ToyName.textContent = toy.name;
+    toyDivCard.appendChild(h2ToyName);
+
+    const imgAvatar = document.createElement('img');
+    imgAvatar.setAttribute("src", toy);
+    imgAvatar.classList.add('toy-avatar') = toy.image;
+    toyDivCard.appendChild(imgAvatar);
+
+    const pLikes = document.createElement('p');
+    pLikes.textContent = toy.likes;
+    toyDivCard.appendChild(pLikes);
+
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('like-btn');
+    toyDivCard.appendChild(likeButton);
+
+    div.appendChild(toyDivCard);
+
+  })
+}
