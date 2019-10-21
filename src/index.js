2ca5a9de55b9f730d01ecdb482dc4426e6167140
AlwinaO <aoyewole2586@gmail.com>
2019-10-21 15:48:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -11,9 +11,12 @@ let addToy = false
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
+  // click will make addToy true
   addToy = !addToy
+  // if true the toyForm will display
   if (addToy) {
     toyForm.style.display = 'block'
+  // if false the toyForm will not display
   } else {
     toyForm.style.display = 'none'
   }
@@ -29,20 +32,22 @@ const fetchToys = () => {
     .then((json) => renderToys(json))
 }
 
-const renderToys = (json) => {
-  const div = document.getElementById('toy-collection');
-  Object.keys(json).forEach((toy) => {
-    console.log(toy); //logs 0 in console or if toy["name"] or toy.name, it's undefined
+const renderToys = (toys) => {
+  const toyCollection = document.getElementById('toy-collection');
+  console.log("toys", toys);
+  toys.map((toy) => {
+    // console.log("toy", toy)
     const toyDivCard = document.createElement('div');
-    toy = toyDivCard.classList.add('toy-card');
+    toyDivCard.classList.add('card');
+    toyDivCard.style.backgroundColor = "red";
 
     const h2ToyName = document.createElement('h2');
     h2ToyName.textContent = toy.name;
     toyDivCard.appendChild(h2ToyName);
 
     const imgAvatar = document.createElement('img');
-    imgAvatar.setAttribute("src", toy);
-    imgAvatar.classList.add('toy-avatar') = toy.image;
+    imgAvatar.setAttribute("src", toy.image);
+    imgAvatar.classList.add('toy-avatar');
     toyDivCard.appendChild(imgAvatar);
 
     const pLikes = document.createElement('p');
@@ -51,9 +56,35 @@ const renderToys = (json) => {
 
     const likeButton = document.createElement('button');
     likeButton.classList.add('like-btn');
+    likeButton.innerText =  "❤️";
+    likeButton.addEventListener("click", addLike);
     toyDivCard.appendChild(likeButton);
 
-    div.appendChild(toyDivCard);
-
+    toyCollection.appendChild(toyDivCard);
   })
+  // Object.keys(json).forEach((toy) => {
+  //   console.log(toy); //logs 0 in console or if toy["name"] or toy.name, it's undefined
+    // 
+    // toy = 
+}
+
+const addLike = function (likes) {
+  console.log("clicked");
+
+  const requestBody = {likes: likes};
+
+  const configLikes = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(requestBody)
+  }
+
+  return fetch("http://localhost:3000/toys/:id", configLikes)
+    .then((response) => response.text())
+    .then((json) => {
+      console.log(document.getElementById("p"));
+    })
 }
