bfb3451c2a4a5717584c2f6fd9fc74308899f0f6
Rebecca Tong <rtong@buffalo.edu>
2019-10-27 16:02:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -19,27 +19,40 @@ addBtn.addEventListener('click', () => {
 //div -- toy collection
 //create a toy collection with class card and then add it to toy collection div
 
-
-
-function getToys(){
-  return fetch("http://localhost:3000/toys")
+window.addEventListener('DOMContentLoaded', () => {
+  fetch("http://localhost:3000/toys")
   .then(resp => resp.json())
   .then(elements => {
   elements.forEach(element => toyCard(element))
-
   //creating a toycard for each element.
   })
-}
+
+})
 
 function toyCard(element){
+
   var newElement = document.createElement('div');
-  var listToys = document.getElementById('toy-collection')
   newElement.className = "card";
-  newElement.innerHTML = getToys()
+  // var listToys = document.getElementById('toy-collection')
+  newElement.innerHTML = element.name
   listToys.appendChild(newElement)
+  var h = document.createElement('h2')
+
+  // var htext = document.createTextNode(element.name)
+  // new element - cardhtml.
+  //h2 - inner
+  //create each element.
+  // add event listener
+  // create individually append everything into newEelement
+
 
   //h2 with toys name
   // img tag  with src of toys image attribtues
   //p tag with how many likes that toy has
   //button tag with a class like btn
+}
+
+//send post request to save the data of a new toy
+function submitData(){
+  return fetch('http://localhost:3000/toys')
 }
