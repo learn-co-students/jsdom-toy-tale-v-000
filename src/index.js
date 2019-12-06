e6e845cc2a83c02c5d046f7accaa6143af5f7220
srijakumar <srijakumar7@gmail.com>
2019-12-06 13:03:07 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let addToy = false
+/*let addToy = false
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
@@ -13,20 +13,106 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   })
 
+})*/
+
+
+
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+let addToy = false
+let divCollect = document.querySelector('#toy-collection')
+
+function getToys(){
+  return fetch('http://localhost:3000/toys')
+  .then(res => res.json)
+}
+
+function postToys(){
+  fetch('http://localhost:3000/toys', {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      "name": toy_data.name.value,
+      "image": toy_data.image.value,
+      "likes": 0
+    }    )
+  })
+.then(res=>res.json())
+.then((obj_toy)=>{
+  let new_toy = renderToys(obj_toy)
+  divCollect.append(new_toy)
 })
 
 
-
-
-POST http://localhost:3000/toys
-headers:
-{
-  "Content-Type": "application/json",
-  Accept: "application/json"
 }
 
-body: JSON.stringify({
-  "name": "Jessie",
-  "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-  "likes": 0
+
+
+
+function likes(e){
+  e.preventDefault()
+  let more = parseInt(e.target.previousElementSibling.innerText) +1
+
+
+  fetch(`http://localhost:3000/toys/${e.target.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": more
+    })
+  })
+  .then(res=>res.json())
+  .then((like_obj => {
+    e.target.previousElementSibling.innerText = '${more} likes'
+  }))
+}
+
+
+function renderToys(toys){
+  let h2 = document.createElement('h2')
+  h2.innerText = toy.name
+  let img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  img.setAttribute('class', 'toy-avatar')
+  let p = document.createElement('p')
+  p.innerText = '${toy.likes} likes'
+
+  let btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = "like"
+  btn.addEventListener('click', (e) =>{
+    console.log(e.target.dataset);
+    likes(e)
+  })
+
+  let divCard = document.createElement('div')
+  divCard.setAttribute('class', 'card')
+  divCard.append(h2, img, p, btn)
+  divCollect.append(divCard)
+}
+
+addBtn.addEventListener('click', () =>{
+  addToy = !addToy
+  if (addToy){
+    toyForm.style.display = 'block'
+    toyForm.addEventListener('submit', event => {
+      event.preventDefault()
+      postToy(event.target)
+    })
+  } else {
+    toyForm.style.display = 'none'
+  }
+})
+
+getToys().then(toys => {
+  toys.forEach(toy => {
+    renderToys(toy)
+  })
 })
