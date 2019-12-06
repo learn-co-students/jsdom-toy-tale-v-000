709746f25783328375b03eb6d75abe13d1222173
srijakumar <srijakumar7@gmail.com>
2019-12-06 12:14:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -14,3 +14,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
   })
 
 })
+
+
+
+
+POST http://localhost:3000/toys
+headers:
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+}
+
+body: JSON.stringify({
+  "name": "Jessie",
+  "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+  "likes": 0
+})
