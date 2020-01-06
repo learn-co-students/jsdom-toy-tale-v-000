bed0a9c66f7078fb493c6098de5b9b7e38a33b3a
ElvinGarcia <dimix181@gmail.com>
2020-01-06 13:28:37 -0500

    fetch call is initiated on DOMLOAD. Server respose are validated for Array return message

--- src/index.js
+++ src/index.js
@@ -12,5 +12,49 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
+  fetchSource(resource)
 })
+
+
+// ----
+const resource = "http://localhost:3000/toys"
+
+function validateResponse(response){
+  if(!response.ok){
+    throw Error (response.statusText);
+  }
+  return response
+}
+
+function responseAsJson(response){
+  return response.json();
+}
+
+function jsonResponsevalidation(jsonResponse){
+  
+  if (!Array.isArray(jsonResponse)){
+    throw Error("The respose Object is that an Array");
+  }
+  return jsonResponse
+}
+
+function imgExtractor(arr){
+ arr.forEach(element => {
+   console.log(element.name)
+ });
+}
+function logError(error){
+  console.log('un error occured: \n', error);
+}
+
+
+// -- initializes img req.
+function fetchSource(resource){
+  fetch(resource)
+  .then(validateResponse)
+  .then(responseAsJson)
+  .then(jsonResponsevalidation)
+  .then(imgExtractor)
+  // .catch(logError)
+}
+
