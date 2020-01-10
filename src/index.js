c32007a1938f5071ff6f3f55595e57e30517e97c
ajimenez-ias <ajimenez@integralads.com>
2020-01-10 17:58:22 -0500

    add create toy card elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 let addToy = false
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -13,4 +14,49 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   })
 
+  let configurationObject = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    // body: JSON.stringify({
+    //   name: userName,
+    //   email: userEmail
+    // })
+  };
+
+  return fetch('http://localhost:3000/toys', configurationObject).then(function (response) {
+    return response.json();
+  })
+    .then(function (json) {
+      json.message.forEach(object => createToyCard(object))
+    })
+
 })
+
+
+
+
+function createToyCard(obj) {
+  let cardDiv = document.createElement('div')
+  let h2 = document.createElement('h2')
+  let image = document.createElement('img')
+  let paragraph = document.createElement('p')
+  let button = document.createElement('button')
+  let allToys = document.getElementById('toy-collection')
+
+  cardDiv.className = "card"
+  image.src.className = "toy-avatar"
+  button.className = "like-btn"
+  h2.textContent = obj['name']
+  image.src(obj['image'])
+  paragraph.textContent = obj['image']
+  button.textContent = obj['likes']
+
+  cardDiv.appendChild(h2)
+  cardDiv.appendChild(image)
+  cardDiv.appendChild(paragraph)
+  cardDiv.appendChild(button)
+  allToys.appendChild(cardDiv)
+}
