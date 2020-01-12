f7a517c1fbc9bc0e66a2da31c5308c226dd4b49a
Grace S <vocalise@gmail.com>
2020-01-12 13:53:32 -0800

    Added event.preventDefault() as well as passing information differently from form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+const addBtn = document.querySelector('#new-toy-btn');
+const toyForm = document.querySelector('.container');
+var divs = document.querySelector("#toy-collection");
+
 let addToy = false
 
-function createToyList(toysObj) {
-  let toyCollectionDiv = document.getElementById("toy-collection");
-  for(toy in toysObj) {
+
+
+function outputToy(toyObj) {
     let divElement = document.createElement("div");
     let h2Element = document.createElement("h2");
     let imgElement = document.createElement("img");
@@ -10,49 +17,56 @@ function createToyList(toysObj) {
     let buttonElement = document.createElement("button");
     let hiddenElement = document.createElement("hidden")
     divElement.setAttribute("class", "card");
-    toyCollectionDiv.appendChild(divElement);
-    h2Element.textContent = toysObj[toy]["name"];
-    imgElement.src = toysObj[toy]["image"];
+    console.log(divs)
+    divs.appendChild(divElement);
+    h2Element.textContent = toyObj["name"];
+    imgElement.src = toyObj["image"];
     imgElement.setAttribute("class","toy-avatar");
-    pTag.innerHTML = toysObj[toy]["likes"] + " Likes";
+    pTag.innerHTML = toyObj["likes"] + " Likes";
     buttonElement.setAttribute("class", "like-btn")
     buttonElement.textContent = "Like <3"
-    hiddenElement.id = toysObj[toy]["id"]
+    buttonElement.addEventListener("click", function() {
+      increaseLikes(this.parentNode.childNodes[4].id, this.parentNode.childNodes[2].textContent)
+    })
+    hiddenElement.id = toyObj["id"]
     divElement.appendChild(h2Element);
     divElement.appendChild(imgElement);
     divElement.appendChild(pTag);
     divElement.appendChild(buttonElement);
     divElement.appendChild(hiddenElement);
+    console.log(divElement)
   }
-}
 
 function getAllToys() {
   return fetch("http://localhost:3000/toys")
   .then(resp => resp.json())
-  .then(json => {
-    createToyList(json);
-    monitorToyLikes();
+  .then(toyObj => {
+    console.log(toyObj)
+    for (toy of toyObj) {
+      outputToy(toy);
+    }
   })
 }
 
-function submitNewToy(name, imgUrl, num_of_likes) {
-  let toy = {
-    name: name,
-    image: imgUrl,
-    likes: num_of_likes
-  };
-
+function submitNewToy(toy_data) {
   let toyObj = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
-    body: JSON.stringify(toy)
+    body: JSON.stringify({
+      name: toy_data.name.value,
+      image: toy_data.image.value,
+      likes: 0
+    })
   }
 
   return fetch("http://localhost:3000/toys", toyObj)
   .then(resp => resp.json())
+  .then(toyObj => {
+    outputToy(toyObj)
+  })
 }
 
 function outputLikes(json) {
@@ -81,27 +95,21 @@ function increaseLikes(id, likes) {
   })
 }
 
-function monitorToyLikes() {
-  all_likes = document.querySelectorAll(".like-btn");
-  for (like of all_likes) {
-      like.addEventListener("click", function() {
-        increaseLikes(this.parentNode.childNodes[4].id, this.parentNode.childNodes[2].textContent)
-      })
-  }
-}
-document.addEventListener("DOMContentLoaded", ()=>{
-  const addBtn = document.querySelector('#new-toy-btn')
-  const toyForm = document.querySelector('.container')
-
-  getAllToys();
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
     if (addToy) {
+      toyForm.addEventListener("submit", event => {
+        event.preventDefault()
+        submitNewToy(event.target);
+      })
       toyForm.style.display = 'block'
     } else {
       toyForm.style.display = 'none'
     }
   })
 
+  getAllToys();
 })
+
+
