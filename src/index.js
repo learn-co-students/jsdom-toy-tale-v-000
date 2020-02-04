3c50c90bb214ba8bca8ec92a955dfa966f23bb31
William Barela <william.barela.dev@gmail.com>
2020-02-03 21:47:15 -0600

    add toyCards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 let addToy = false;
+let toysEndpoint = "http://localhost:3000/toys"
 
-document.addEventListener("DOMContentLoaded", () => {
+function executeGet(f, url){
+  fetch(url)
+  .then(r => r.json())
+  .then(json => f(json))
+}
+
+function formSetup(){
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +19,43 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+}
+
+function createToyCard(toy){
+  let toyCard = document.createElement('div');
+  let toyName = document.createElement('h2');
+  let toyImg = document.createElement('img');
+  let toyLikes = document.createElement('p');
+  let likeButton = document.createElement('button');
+
+  toyCard.classList.add('card');
+  toyName.innerText = toy.name;
+  toyImg.classList.add('toy-avatar');
+  toyImg.src = toy.image;
+  toyLikes.innerText = `${toy.likes} Likes`;
+  likeButton.classList.add('like-btn');
+  likeButton.innerHTML = 'Like &hearts;';
+
+  const children = [toyName, toyImg, toyLikes, likeButton];
+
+  for(child of children){
+    toyCard.appendChild(child);
+  }
+
+  return toyCard;
+}
+
+function listToys(json){
+  const toyCollection = document.getElementById('toy-collection');
+  
+  for(const toy of json){
+    let toyCard = createToyCard(toy);
+    toyCollection.appendChild(toyCard);
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  formSetup();
+  executeGet(listToys, toysEndpoint);
 });
