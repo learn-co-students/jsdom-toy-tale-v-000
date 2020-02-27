47ec88dfedf51298f48b8fa7eaa8cd36dd94fb17
MiriamPeskowitz <miriam5000@gmail.com>
2020-02-26 19:29:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,69 +1,25 @@
-const addNewToyButton = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-const addToyForm = document.querySelector('.add-toy-form')
 let addToy = false
 
+const url = "http://localhost:3000/toys"
 
+window.addEventListener('DOMContentLoaded', getAllToys)
 
-const addNewToy = (toy) => { //toy = event.target
-    const url = "http://localhost:3000/toys"
-    let configObjectPost = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-
-        body: JSON.stringify({
-            'name': toy.name.value,
-            'image': toy.image.value,
-            'likes': 0
-        })
-    }
-
-	fetch(url, configObjectPost )
-		.then(response => response.json())
-		.then(data => {
-			createToyCard(data)
-		})
-		.catch(error => console.log(error.message))
+function getAllToys() {
+  fetch(url)
+	.then(response => response.json())
+	.then(json => listAllToys(json))
 }
-
-addNewToyButton.addEventListener('click', (event) => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block';
-    //do something to capture the data and go to POST/
-    addToyForm.addEventListener('submit', (e) => {
-    	e.preventDefault()
-	    addNewToy(e.target)
-		})
-  	} else {
-    toyForm.style.display = 'none'
-  }
-})
-
-//Fetch and display the toys
-const getAllToys = () => {
-	 fetch('http://localhost:3000/toys')
-		.then(response => response.json())
-	
-		.then(json => listAllToys(json))
-}
-
 
 const listAllToys = (toys) => {
-	toys.forEach(toy => createToyCard(toy))
+  toys.forEach(toy => createToyCard(toy))
 }
 
-const createToyCard = (toy) => {
 
+const createToyCard = (toy) => {
 	let div = document.createElement('div')
 	div.className = "card"
 	div.setAttribute('id', toy.id)
 	
-
 	let h2 = document.createElement('h2')
 	h2.innerText = `${toy.name}`
 
@@ -78,9 +34,10 @@ const createToyCard = (toy) => {
 
 	let button = document.createElement('button')
 	button.setAttribute('class', 'like-button')
-	// button.setAttribute('id', toy.id)
+	button.setAttribute('id', toy.id)
 	button.innerText = 'Like'
 	button.addEventListener('click', (event) => {
+		console.log("event.target.dataset: ", event.target.dataset)
 		updateLikes(event)
 	})
 
@@ -90,26 +47,20 @@ const createToyCard = (toy) => {
 	
 }
 
-window.addEventListener('DOMContentLoaded', getAllToys)
-
-// click button -- call updateLikes function, carry the event object over 
-
 
 const updateLikes = (event) => {
 	event.preventDefault()
 	const likes = event.target.previousElementSibling
 	const newLikes = parseInt(likes.innerHTML) + 1;
 	const id = likes.id
-	// const id = event.target.id
-	// let putLikesHere = document.getElementsByTagName(`p#${id}`)
 	
 	console.log(event.target)
 	console.log('id: ', event.target.id)
-	// console.log("event target: " , event.target) 
 	console.log("prevSibl: ", likes.innerHTML)
 	
 
 	const url = `http://localhost:3000/toys/${id}`
+	
 	const configObjectPatch = {
         method: "PATCH",
         headers: {
@@ -120,7 +71,7 @@ const updateLikes = (event) => {
             'likes': newLikes////figure out how to capture current likes 
         })
     }
-    console.log(newLikes)
+    console.log("newLikes: ", newLikes)
 	fetch(url, configObjectPatch)
 		.then(response => response.json())
 		.then(data => {(data.likes)
@@ -132,9 +83,48 @@ const updateLikes = (event) => {
 
 }
 
-// How to update the p/number right away -- why isn't it? 
-// I'm not always clear about what props and arguments
-//I'm passing from function to function.
+const addNewToy = (toy) => { //toy = event.target
+     
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            'name': toy.name.value,
+            'image': toy.image.value,
+            'likes': 0
+        })
+    }
+
+  fetch(url, configObject)
+	.then(response => response.json())
+	.then(data => {
+		createToyCard(data)
+	})
+	.catch(error => console.log(error.message))
+}
+
+
+ addNewToyButton = document.querySelector('#new-toy-btn')
+  addNewToyButton.addEventListener('click', (event) => {
+  // hide & seek with the form
+    const toyForm = document.querySelector('.container')
+
+  	addToy = !addToy
+  	if (addToy) {
+   	  toyForm.style.display = 'block';
+    //do something to capture the data and go to POST/
+      const addToyForm = document.querySelector('.add-toy-form')
+      addToyForm.addEventListener('submit', (event) => {
+    	event.preventDefault()
+	    addNewToy(event.target)
+	 	})
+	 	} else {
+    	toyForm.style.display = 'none'
+  	 }
+  })
 
 
 	
