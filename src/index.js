4deb4d2724d30048c83e17259713f4457fafb219
sjwakeman <swakeman@ca.rr.com>
2020-04-11 20:44:49 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // When the page loads, make a 'GET' request to fetch all the toy objects.
 window.onload = () => {
   fetchAllToys();
+  // toyInfo()
 
 }
-
 
 let addToy = false;
 
@@ -21,33 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function fetchAllToys() {
-  const allToysListUrl = 'http://localhost:3000/toys'
-  //on page load, fetch all the dog breeds using the url above 
-  fetch(allToysListUrl)
-  .then(resp => resp.json())
-      .then(json => renderToys(json.message))
-      console.log(message)
-}
-
-// function renderToys(message) {
-//   allToys = document.getElementById("toy-collection")
-//     originalToyList = Object.keys(message) //Original Toy List
-//     // iterate over the array
-//     for(let i = 0; i < originalToyList.length; i++) {
-//       allToys.innerHTML +=`<li>${originalToyList[i]}</li>`
-//     }
-    
-// }
-
-
-// function fetchAllToys() {
-//   return fetch("http://localhost:3000/toys")
-//     .then(resp =>resp.json)
-// }      
-// fetchAllToys()
-
-
+// TEMPLATE
 // fetch("string representing a URL to a data source")
 // fetch("http://localhost:3000/toys") 
 //   //  .then(function(response) {
@@ -61,30 +35,81 @@ function fetchAllToys() {
 //     document.getElementById("toy-collection").container.add("card");
 //   })
 
+function fetchAllToys() {
+  const allToysListUrl = 'http://localhost:3000/toys'
+  //on page load, fetch all the dog breeds using the url above 
+  fetch(allToysListUrl)
+  .then(resp => resp.json())
+      .then(json => renderToys(json))
+      // console.log(json)
+}
+
+// function renderToys(json) {
+//   allToys = document.getElementById("toy-collection")
+//     originalToyList = Object.keys(json) //Original Toy List
+//     // iterate over the array
+//     for(let i = 0; i < originalToyList.length; i++) {
+//       allToys.innerHTML +=`<li>${originalToyList[i]}</li>`
+//     }
+    
+// }
+
+function toyInfo(){
+  // create Toy Elements
+  const card = document.createElement("div")
+  const h2 = document.createElement("h2")
+  const img = document.createElement("img")
+  const p = document.createElement("p")
+  const button = document.createElement("btn")
+  // h2 tag with the toy's name
+  let toysName = h2
+  card.appendChild(toysName)
+  // img tag with the src of the toy's image attribute and the class name "toy-avatar"
+  let toyImage = img 
+  card.appendChild(toyImage)
+  // p tag with how many likes that toy has
+  let toyLikes = p
+  card.appendChild(toyLikes)
+  // button tag with a class "like-btn"
+  let likeButton = button
+  card.appendChaild(likeButton)
+
+  document.querySelector("toy-collection")
+  // Set to updateToyList
+  updateToyList = document.querySelector("toy-collection")
+
+}
 
 
-// function fetchAllToys() {
+function renderToys(json) {
+  // debugger //HIT
+  toyCollection = document.getElementById("toy-collection")
+  // debugger //HIT
+  originalToyList = Object.keys(json) //Original Toy List
+  // debugger //HIT 
+    // iterate over array
+    for (let i = 0; i < originalToyList.length; i++) {
+    // debugger //HIT
+      toyCollection.innerHTML += `<li>${originalToyList[i]}</li>`
+    } 
+    // debugger //Hit
+}
+
 //   const toysUrl = "http:localhost:3000/toys"
 //     fetch(toysUrl)
 //       .then(resp => resp.json())
 //       .then(json => {
-//         document.getElementById("toy-collection")
-//         toysList = document.getElementById("toy-collection")
-//           for (let i = 0; i < json.message.length; i++) {
-//             document.getElementById("toy-collection").container.add("card");
-// }
+        toyCollection = document.getElementById("toy-collection")
+  
+  // Add class name to the div
+  // card.className = "card"
+  // const h2 = document.createElement('h2')
 
-// function fetchAllToys() {
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(json){
-//       // Use this data inside of `json` to do DOM manipulation
-//     })
-//   // With the response data, make a <div class="card"> for each toy and add it to the toy-collection div.
-//   document.getElementById("toy-collection")
-// document.getElementById("toy-collection").container.add("card");
-// }
+//         toysList = document.getElementById("toy-collection")
+//           for (let i = 0; i < json.json.length; i++) {
+//             document.getElementById("toy-collection").container.add("card");
+
+
 
 
 // function addClass(selector, card) {
@@ -97,3 +122,13 @@ function fetchAllToys() {
 //     elements[i]."toy-collection".add(card);
 //   }
 // }
+
+function addCard() {
+    // Add class name to the div
+    card.className = "card"
+}
+
+function addButton() {
+  // Add class name to the button
+  button.className = "like=btn"
+}
