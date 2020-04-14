eb9155450741787f2cc4a3389a2e56172ead4c20
sjwakeman <swakeman@ca.rr.com>
2020-04-14 15:24:28 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ window.onload = () => {
 
 }
 
+const allToysListUrl = 'http://localhost:3000/toys'
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -21,27 +22,18 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// TEMPLATE
-// fetch("string representing a URL to a data source")
-// fetch("http://localhost:3000/toys") 
-//   //  .then(function(response) {
-//   .then(function fetchAllToys(response) {
-//     // return response.json();
-//     return response.json();
-//   })
-//   .then(function(json){
-//     // Use this data inside of `json` to do DOM manipulation
-//     // With the response data, make a <div class="card"> for each toy and add it to the toy-collection div.
-//     document.getElementById("toy-collection").container.add("card");
-//   })
 
 function fetchAllToys() {
-  const allToysListUrl = 'http://localhost:3000/toys'
+  // const allToysListUrl = 'http://localhost:3000/toys' //MADE GLOBAL VARIABLE
   //on page load, fetch all the toys using the url above 
-  fetch(allToysListUrl)
+  return fetch(allToysListUrl)
+  
   .then(resp => resp.json())
-      .then(json => renderToys(json))
-      // console.log(json)
+  
+  .then(json => json.forEach(toy => {
+    renderToys(toy)
+  }))
+
 }
 
 // function renderToys(json) {
@@ -55,20 +47,66 @@ function fetchAllToys() {
 // }
 
 
-function renderToys(json) {
+function renderToys(toy) {
   // debugger //HIT
-  toyCollection = document.getElementById("toy-collection")
-  // debugger //HIT
-  originalToyList = Object.keys(json) //Original Toy List
-  // debugger //HIT 
-    // iterate over array
-    for (let i = 0; i < json.length; i++) {
-      toyInfo(json[i])
-    // for (let i = 0; i < originalToyList.length; i++) {
-    // debugger //HIT.
-    // toyCollection.innerHTML += `<li>${json[i].name}</li>`
-      // toyCollection.innerHTML += `<li>${originalToyList[i]}</li>`
-    } 
+  console.log(toy)
+
+
+    // create Toy Elements
+    const card = document.createElement("div")
+    // h2 tag with the toy's name
+    const h2 = document.createElement("h2")
+    card.className = "card"
+    //  Displays toy name
+    toyName = toy.name
+    card.appendChild(h2).toyName
+    // img tag with class name "toy-avatar"
+    const img = document.createElement("toy-avatar")
+    img.className = "toy-avatar"
+    // img tag with the src of the toy's image attribute
+    toyImage = toy.image
+    card.appendChild(img).toyImage
+    // p tag with how many likes that toy has
+    const p = document.createElement("p")
+    // p tag with how many likes that toy has
+    toyLikes = toy.likes
+    card.appendChild(p).toyLikes
+    // button tag with a class "like-btn"
+    const button = document.createElement("like-btn")
+    button.className = "like=btn"
+    likeButton = button.innerHTML
+    card.appendChild(button).likeButton 
+   
+    document.querySelector("toy-collection")
+    // Set to updateToyList
+    return updateToyList = document.querySelector("toy-collection")
+    // return updateToyList()
+
+
+
+
+ 
+//   for (let i = 0; i < toy.length; i++) {
+//     // debugger //HIT.
+//     toyInfo(json[i])
+//   // ==debugger //HIT.
+//  // console.log(json)
+// //  console.log("what is i", json[i])
+//   return fetch(allToysListUrl)
+// .then(resp => resp.json())
+// .then(json => renderToys(json))
+//   // toyCollection.innerHTML += `<li>${json[i].name}</li>`
+//     // toyCollection.innerHTML += `<li>${originalToyList[i]}</li>`
+//   } 
+  // toyCollection = document.getElementById("toy-collection")
+  //   toyCollection.innerHTML += `<li>${json[i].name}</li>` //index.js:49 Uncaught (in promise) ReferenceError: i is not defined
+  //   toyCollection.innerHTML += `<li>${originalToyList[i]}</li>`
+  // // debugger //HIT
+  // originalToyList = Object.keys(json) //Original Toy List
+  // // debugger //HIT 
+  //   // iterate over array
+  //   console.log(json)
+
     //  debugger //Hit
 }
 
@@ -100,20 +138,20 @@ function renderToys(json) {
 //   }
 // }
 
-function addCard() {
-    // Add class name to the div
-    card.className = "card"
-}
+// function addCard() {
+//     // Add class name to the div
+//     card.className = "card"
+// }
 
-function addButton() {
-  // Add class name to the button
-  button.className = "like=btn"
-}
+// function addButton() {
+//   // Add class name to the button
+//   button.className = "like=btn"
+// }
 
-function addImage() {
-  // Add class name to the image
-  card.className = "toy-avatar"
-}
+// function addImage() {
+//   // Add class name to the image
+//   card.className = "toy-avatar"
+// }
 
 function addNewToy(toy) {
   //  a POST request is sent to http://localhost:3000/toys
@@ -122,74 +160,50 @@ function addNewToy(toy) {
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
-    }
+    },
+    body: JSON.stringify({
+      "name": toyInfo(toy).name.value,        //add the initial data IE THE FORM
+      "image": toyInfo(toy).image.value,      //"https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+      "likes": toyInfo(toy).toyLikes.value    //0
+    })  
+    // .then(resp => ).the name("input-text")
+    // .then(resp => ). the image("input-text")
+
+    // ADD TO THE DOM
   })
-
- 
-  body: JSON.stringify({
-    "name": toyInfo.name.value,        //"Jessie",
-    "image": toyInfo.image.value,      //"https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-    "likes": toyInfo.toyLikes.value    //0
-  })  
-} 
-
-function toyInfo(toy){
-  // create Toy Elements
-  const card = document.createElement("div")
-  // h2 tag with the toy's name
-  const h2 = document.createElement("h2")
-  // const img = document.createElement("img")
-  // const img = document.createElement("img").src
-  // const img = document.createElement("toy-avatar").src
-  // img tag with the src of the toy's image attribute and the class name "toy-avatar"
-  const img = document.createElement("toy-avatar")
-  const p = document.createElement("p")
-  const button = document.createElement("like-btn")
-
-  // h2 tag with the toy's name
-    //  Displays toy name
-    toyName = toy.name
-    card.appendChild(h2).toyName
-  //  debugger //HITS
-  //  card.appendChild(h2).toyName
-  // h2.innerText=toy.name
-  // card.appendChild(toyName)
-  // card.appendChild(h2).toyName
-  // toyName = document.card.getElementById("h2")
-  // debugger //HITS
-
-  // img tag with the src of the toy's image attribute and the class name "toy-avatar"
-  toyImage = toy.image
-  card.appendChild(img).toyImage
-    // let toyImage = img.innerHTML
-  // card.appendChild(toyImage)
-  // toyImage = toy.image.href
-  // toyImage = toy.image.target
-     // card.appendChild("toy-avatar") index.js:159 Uncaught (in promise) TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
-  // card.appendChild("img").toyImage //index.js:154 Uncaught (in promise) TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
-  // card.appendChild(img).toyImage.href index.js:160 Uncaught (in promise) TypeError: Cannot read property 'href' of undefined
-  // debugger // HITS
-
-
-  // p tag with how many likes that toy has
-  toyLikes = toy.likes
-  // debugger //HITS
-  // toyLikes = p.innerHTML
-  card.appendChild(p).toyLikes
-  // debugger //HITS   
-    // toyLikes = toy.likes //index.js:161 Uncaught (in promise) ReferenceError: Cannot access 'toyLikes' before initialization
-    // card.appendChild(img).toyImage.href
-    // debugger //DOES NOT HIT
-  
-  // button tag with a class "like-btn"
-  likeButton = button.innerHTML
-  // debugger //HITS
-  card.appendChild(button).likeButton 
-  // debugger //HITS
-
-
-  document.querySelector("toy-collection")
-  // Set to updateToyList
-  updateToyList = document.querySelector("toy-collection")
-
 }
+
+// function toyInfo(toy){
+//   // create Toy Elements
+//   const card = document.createElement("div")
+//   // h2 tag with the toy's name
+//   const h2 = document.createElement("h2")
+
+//   const img = document.createElement("toy-avatar")
+//   const p = document.createElement("p")
+//   const button = document.createElement("like-btn")
+
+//   // h2 tag with the toy's name
+//     //  Displays toy name
+//     toyName = toy.name
+//     card.appendChild(h2).toyName
+ 
+//   // img tag with the src of the toy's image attribute and the class name "toy-avatar"
+//   toyImage = toy.image
+//   card.appendChild(img).toyImage
+   
+//   // p tag with how many likes that toy has
+//   toyLikes = toy.likes
+ 
+//   card.appendChild(p).toyLikes
+ 
+//   // button tag with a class "like-btn"
+//   likeButton = button.innerHTML
+//   card.appendChild(button).likeButton 
+ 
+//   // document.querySelector("toy-collection")
+//   // Set to updateToyList
+//   return updateToyList = document.querySelector("toy-collection")
+//   // return updateToyList()
+
+// }
