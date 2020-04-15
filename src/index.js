33f23fb07682937a38937af4f299e9fedb05cdb4
sjwakeman <swakeman@ca.rr.com>
2020-04-15 14:47:41 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 function fetchAllToys() {
-  // const allToysListUrl = 'http://localhost:3000/toys' //MADE GLOBAL VARIABLE
   //on page load, fetch all the toys using the url above 
   return fetch(allToysListUrl)
   
@@ -45,121 +44,86 @@ function fetchAllToys() {
 
 
 function renderToys(toy) {
-  // debugger //HIT
+  // DISPLAYS EACH TOY SEPERATELY
   console.log(toy)
 
 
-    // create Toy Elements
+    // create card for each toy
     const card = document.createElement("div")
     card.className = "card"
     // CARD HAS TO BE APPENDED TO THE NODE ON THE WEBPAGE
     document.getElementById("toy-collection").appendChild(card)
 
-    // h2 tag with the toy's name
-    const h2 = document.createElement("h2")
-    // card.className = "card"
-    //  Displays toy name
-    toyName = toy.name
-    card.appendChild(h2).toyName
-    // img tag with class name "toy-avatar"
-    const img = document.createElement("toy-avatar")
-    img.className = "toy-avatar"
-    // img tag with the src of the toy's image attribute
-    toyImage = toy.image
-    card.appendChild(img).toyImage
-    // p tag with how many likes that toy has
-    const p = document.createElement("p")
-    // p tag with how many likes that toy has
-    toyLikes = toy.likes
-    card.appendChild(p).toyLikes
-    // button tag with a class "like-btn"
-    const button = document.createElement("like-btn")
-    button.className = "like=btn"
-    likeButton = button.innerHTML
-    card.appendChild(button).likeButton 
 
-    //document.getElementById("new-toy-btn").addEventListener('click', function)// function()), updateList()), addNewToy(toy))
-    // document.getElementById("breed-dropdown").addEventListener('change', removeBreeds) 
+    // CREATE TOY ELEMENTS
+
+    // h2 tag with the toy's name
+    //Create  h2 tag 
+    const h2 = document.createElement("h2")
+
+    //  Set toyName
+    toyName = toy.name
+
+    // Adds the innerHTML of the h2 to toyName
+    // h2.setAttribute("innerHTML", toyName)
+
+    // Sets the innerHTML of h2 to toyName
+    h2.innerHTML = toyName
+    // Appends child "h2" to the parent "card"
+    card.appendChild(h2)
+
+    // img tag with the src of the toy's image attribute and the class name "toy-avatar"
+    // Create img tag
+    const img = document.createElement("img")
+   
+     // img tag with class name "toy-avatar"
+    img.className = "toy-avatar"
+
+    // Sets toyImage
+    toyImage = toy.image
+
+    // Sets img attribute "src" to toyImage
+     img.src = toyImage
+
+    //  Sets img attribute "src" to toyImage
+     // img.setAttribute("src", toyImage) //toy-avatar section on card
+
+    // Appends child "img" to the parent "card"
+    card.appendChild(img)
+
+    // p tag with how many likes that toy has
+    // Creates p tag
+    const p = document.createElement("p")
+
+    // Sets toyLikes
+    toyLikes = toy.likes
+
+    // Adds the innerHTML to the p tag to toyLikes
+    p.innerHTML = toyLikes
+
+    // Appends child "p" to the parent "card"-btn
+    card.appendChild(p)
+    
+    // button tag with a class "like-btn"
+    // Create button
+    const button = document.createElement("like-btn")
+
+    // button with a class name of "like"
+    button.className = "like-btn"
+
+    // Sets likeButton
+    likeButton = button
+
+    // Adds the innerHTML to the blikeButton to button
+    likeButton.innerHTML = button //[object HTMLElement]
+
+     // Sets img attribute "src" to toyImage
+    //  button.src = likeButton
+
+    // Appends child "button" to the parent "card"
+    card.appendChild(button)
 }  
   
-//   function updateList()
-//     // document.querySelector("toy-collection")
-//     // Set to updateToyList
-//     // updateToyList = document.querySelector("toy-collection")
-//     updateToyList = allToyListUrl.append('card')
-//     return updateToyList
-// }
-
-
-
- 
-//   for (let i = 0; i < toy.length; i++) {
-//     // debugger //HIT.
-//     toyInfo(json[i])
-//   // ==debugger //HIT.
-//  // console.log(json)
-// //  console.log("what is i", json[i])
-//   return fetch(allToysListUrl)
-// .then(resp => resp.json())
-// .then(json => renderToys(json))
-//   // toyCollection.innerHTML += `<li>${json[i].name}</li>`
-//     // toyCollection.innerHTML += `<li>${originalToyList[i]}</li>`
-//   } 
-  // toyCollection = document.getElementById("toy-collection")
-  //   toyCollection.innerHTML += `<li>${json[i].name}</li>` //index.js:49 Uncaught (in promise) ReferenceError: i is not defined
-  //   toyCollection.innerHTML += `<li>${originalToyList[i]}</li>`
-  // // debugger //HIT
-  // originalToyList = Object.keys(json) //Original Toy List
-  // // debugger //HIT 
-  //   // iterate over array
-  //   console.log(json)
-
-    //  debugger //Hit
-// }
-
-//   const toysUrl = "http:localhost:3000/toys"
-//     fetch(toysUrl)
-//       .then(resp => resp.json())
-//       .then(json => {
-        // toyCollection = document.getElementById("toy-collection")
-  
-  // Add class name to the div
-  // card.className = "card"
-  // const h2 = document.createElement('h2')
-
-//         toysList = document.getElementById("toy-collection")
-//           for (let i = 0; i < json.json.length; i++) {
-//             document.getElementById("toy-collection").container.add("card");
-
-
-
-
-// function addClass(selector, card) {
-
-//   // get all elements that match our selector
-//   elements = document.querySelectorAll("toy-collection");
-
-//   // add class to all chosen elements
-//   for (var i=0; i<elements.length; i++) {
-//     elements[i]."toy-collection".add(card);
-//   }
-// }
-
-// function addCard() {
-//     // Add class name to the div
-//     card.className = "card"
-// }
-
-// function addButton() {
-//   // Add class name to the button
-//   button.className = "like=btn"
-// }
-
-// function addImage() {
-//   // Add class name to the image
-//   card.className = "toy-avatar"
-// }
-
 function addNewToy(toy) {
   //  a POST request is sent to http://localhost:3000/toys
   fetch("http://localhost:3000/toys", {
@@ -179,38 +143,3 @@ function addNewToy(toy) {
     // ADD TO THE DOM
   })
 }
-
-// function toyInfo(toy){
-//   // create Toy Elements
-//   const card = document.createElement("div")
-//   // h2 tag with the toy's name
-//   const h2 = document.createElement("h2")
-
-//   const img = document.createElement("toy-avatar")
-//   const p = document.createElement("p")
-//   const button = document.createElement("like-btn")
-
-//   // h2 tag with the toy's name
-//     //  Displays toy name
-//     toyName = toy.name
-//     card.appendChild(h2).toyName
- 
-//   // img tag with the src of the toy's image attribute and the class name "toy-avatar"
-//   toyImage = toy.image
-//   card.appendChild(img).toyImage
-   
-//   // p tag with how many likes that toy has
-//   toyLikes = toy.likes
- 
-//   card.appendChild(p).toyLikes
- 
-//   // button tag with a class "like-btn"
-//   likeButton = button.innerHTML
-//   card.appendChild(button).likeButton 
- 
-//   // document.querySelector("toy-collection")
-//   // Set to updateToyList
-//   return updateToyList = document.querySelector("toy-collection")
-//   // return updateToyList()
-
-// }
