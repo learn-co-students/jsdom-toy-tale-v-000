4e23c47674465b0ca4cba1b121cedd04bc6b4660
sjwakeman <swakeman@ca.rr.com>
2020-04-20 16:15:38 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -58,39 +58,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function fetchAllToys() {
   //on page load, fetch all the toys using the url above 
-  return fetch(allToysListUrl)
-  
-  .then(resp => resp.json())
-  
-  .then(json => json.forEach(toy => {
-    renderToy(toy)
-  }))
-
+  fetch(allToysListUrl)
+    .then(resp => resp.json() )
+    .then(json => json.forEach(toy => renderToy(toy)))
 }
 
-// function renderToy(json) {
-//   allToys = document.getElementById("toy-collection")
-//     originalToyList = Object.keys(json) //Original Toy List
-//     // iterate over the array
-//     for(let i = 0; i < originalToyList.length; i++) {
-//       allToys.innerHTML +=`<li>${originalToyList[i]}</li>`
-//     }
-    
-// }
-
-
 function renderToy(toy) {
-
-    // create card for each toy
+  // create card for each toy
     const card = document.createElement("div")
-    card.className = "card"
-    // CARD HAS TO BE APPENDED TO THE NODE ON THE WEBPAGE
-    document.getElementById("toy-collection").appendChild(card)
+      card.className = "card"
 
-
-    // CREATE TOY ELEMENTS
-
-    // h2 tag with the toy's name
+  // CREATE TOY ELEMENTS
+  // h2 tag with the toy's name
     //Create  h2 tag 
     const h2 = document.createElement("h2")
 
@@ -100,60 +79,63 @@ function renderToy(toy) {
     // Adds the innerHTML of the h2 to toyName
     h2.innerHTML = toyName
     // Appends child "h2" to the parent "card"
-    card.appendChild(h2)
+      card.appendChild(h2)
 
-    // img tag with the src of the toy's image attribute and the class name "toy-avatar"
-    // Create img tag
+  // Create img tag
     const img = document.createElement("img")
    
      // img tag with class name "toy-avatar"
     img.className = "toy-avatar"
 
-    // Sets toyImage
-    toyImage = toy.image
+      // Sets toyImage
+      toyImage = toy.image
 
-    // Sets img attribute "src" to toyImage
-     img.src = toyImage
+      // Sets img attribute "src" to toyImage
+      img.src = toyImage
 
-    // Appends child "img" to the parent "card"
-    card.appendChild(img)
+        // Appends child "img" to the parent "card"
+        card.appendChild(img)
 
-    // p tag with how many likes that toy has
-    // Creates p tag
-    const p = document.createElement("p")
+      // p tag with how many likes that toy has
+      // Creates p tag
+      const p = document.createElement("p")
 
-    // Sets toyLikes
-    toyLikes = toy.likes
+        // Sets toyLikes
+        toyLikes = toy.likes
 
-    // Adds the innerHTML to the p tag to toyLikes
-    p.innerHTML = toyLikes
+        // Adds the innerHTML to the p tag to toyLikes
+        p.innerHTML = toyLikes
 
-    // Appends child "p" to the parent "card"-btn
-    card.appendChild(p)
+          // Appends child "p" to the parent "card"-btn
+        card.appendChild(p)
     
     // button tag with a class "like-btn"
     // Create button
     const button = document.createElement("button")
 
-    // button with a class name of "like"
-    button.className = "like-btn"
+      // button with a class name of "like"
+      button.className = "like-btn"
 
-    // Sets likeButton
-    likeButton = button
-    likeButton.innerHTML = "Like"
-    // likeButton.src = button // nothing displayed
+      // Sets likeButton
+      likeButton = button
+    
+        // Adds the innerHTML of the likeButton to "Like"
+        likeButton.innerHTML = "Like"
 
-     // Sets img attribute "src" to toyImage
-    button.src = likeButton
+        // Sets img attribute "src" to toyImage
+        button.src = likeButton
 
-    // Appends child "button" to the parent "card"
-    card.appendChild(button)
+          // Appends child "button" to the parent "card"
+          card.appendChild(button)
+
+      // CARD HAS TO BE APPENDED TO THE NODE ON THE WEBPAGE
+       document.getElementById("toy-collection").appendChild(card)
 }  
   
-  function addNewToy(toyForm) {
-    event.preventDefault(); 
+  function addNewToy(event) {
+ 
 
-  const e = []
+  
   // debugger //HITS
   //  a POST request is sent to http://localhost:3000/toys
   fetch("http://localhost:3000/toys", {
@@ -164,15 +146,18 @@ function renderToy(toy) {
       "Accept": "application/json"
     },
     body: JSON.stringify({
-      name: toyForm.target[0].value, // Test
-      image: toyForm.target[1].value // https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist"
+      name: event.target[0].value, // Test
+      image: event.target[1].value // https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist"
     })
 
 // THIS POPULATES THE db.json WITH THE NEW TOY BUT DOES NOT DISPLAY THE TOY ON THE WEBPAGE ORINALLY!
 // UPON REFRESH THE NEW TOY DISPLAYS ON THE WEBPAGE
+    
+    
 
-    .then(resp => renderToy())
   })
+    .then(resp => resp.json())
+    .then(resp => renderToy(resp))
 }
 
 
