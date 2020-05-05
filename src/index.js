c5147a56edc75450af868ea21a9002310cb721fe
iamtamtamiam <vutam_a@hotmail.com>
2020-05-05 18:29:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,61 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+//fecth the toys first
+
+function fetchToys() {
+  return fetch("http://localhost:3000/toys")
+    .then(function(response) {
+      return response.json();
+    })
+
+    .then(function(json) {
+      let fetchedToys = json
+      displayToys(fetchedToys)
+    });
+};
+
+//add divs to each toy
+
+function displayToys(fetchedToys) {
+  fetchedToys.forEach(toy => {
+    let tCollection = document.getElementById("toy-collection")
+    let addedDiv = document.createElement('div')
+    addedDiv.classList.add('card')
+    addedDiv.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    `
+
+    //add like <p> and set variable for count
+    let numberOfLikes = document.createElement('p')
+    numberOfLikes.innerHTML = `${toy.likes}`
+    addedDiv.append(numberOfLikes)
+
+      //add a Like button, addEvent, and append
+      let likeBtn = document.createElement('button')
+        likeBtn.class = 'like-btn'
+        likeBtn.innerText = 'Like <3'
+
+        let addLike = function() {
+          addingLikes(numberOfLikes);
+        } 
+      
+        likeBtn.addEventListener("click", addLike);
+
+        addedDiv.append(likeBtn)
+
+    tCollection.append(addedDiv)
+  })
+}
+
+//adding plus one like 
+function addingLikes(numberOfLikes) {
+  let newNumber = parseInt(numberOfLikes.innerHTML);
+  newNumber++;
+  numberOfLikes.innerHTML = newNumber;
+};
+
+
+
