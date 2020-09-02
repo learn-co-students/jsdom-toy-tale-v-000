cd3c0c6a22ea23b7e9ded97b928294463272296e
Eric Johnson <ericjohnson0216@icloud.com>
2020-09-01 22:34:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+ 
+ 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
