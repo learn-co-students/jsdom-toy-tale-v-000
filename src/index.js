8c4d0883b7d95c2859408c616b3e87df9f019fc9
klovae <elyse.klova@gmail.com>
2021-04-19 23:01:13 -0400

    toy card functionality added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  getToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +13,47 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
 });
+
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json) {
+    const toyArray = json
+    for (const toy of toyArray) {
+      createToys(toy)
+    }
+  })
+}
+
+const toyCollection = document.getElementById('toy-collection')
+
+function createToys(toyObject) {
+  let toyCard = document.createElement('div');
+  toyCard.className = 'card';
+
+  let toyName = document.createElement('h2');
+  toyName.innerText = toyObject['name'];
+  toyCard.appendChild(toyName);
+
+  let toyImg = document.createElement('img');
+  toyImg.src = toyObject['image'];
+  toyImg.className = 'toy-avatar'
+  toyCard.appendChild(toyImg);
+
+  let toyLikes = document.createElement('p')
+  toyLikes.innerHTML = toyObject['likes'] + " Likes"
+  toyCard.appendChild(toyLikes)
+
+  let toyButton = document.createElement('button')
+  toyButton.className = 'like-btn'
+  toyButton.innerText = 'Like <3'
+  toyCard.appendChild(toyButton)
+
+  toyCollection.appendChild(toyCard)
+}
+
